62dc5dddcfa820d7a8c42b9ed789af2d8db82cd5
hecey <hecey@hotmail.com>
2017-11-13 16:46:39 -0500

    HttpURLConnection

--- app/src/main/java/com/example/katrina/imagedownloader/ImagedownloaderActivity.java
+++ app/src/main/java/com/example/katrina/imagedownloader/ImagedownloaderActivity.java
@@ -1,7 +1,12 @@
 package com.example.katrina.imagedownloader;
 
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -17,69 +22,87 @@ import android.widget.Toast;
 
 public class ImagedownloaderActivity extends Activity {
 
-	private String imageUrls[] = {"http://corrupteddevelopment.com/wp-content/uploads/2012/02/google-maps-icon.jpg",
-								"http://sarro.com.br/blog/wp-content/upload/google-logo-novo.jpg",
+    private String imageUrls[] = {"http://corrupteddevelopment.com/wp-content/uploads/2012/02/google-maps-icon.jpg",
+            "http://sarro.com.br/blog/wp-content/upload/google-logo-novo.jpg",
 
-								};
+    };
 
-	private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
-		  ImageView bmImage;
+    //https://developer.android.com/reference/java/net/HttpURLConnection.html
+    private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
 
-		  public ImageDownloader(ImageView bmImage) {
-		      this.bmImage = bmImage;
-		  }
+        public ImageDownloader(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
 
-		  protected Bitmap doInBackground(String... urls) {
-		      String url = urls[0];
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap bitmap = null;
+            URL url1 = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url1 = new URL(url);
+                urlConnection = (HttpURLConnection) url1.openConnection();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-			  Bitmap bitmap = null;
-		      try {
-		        InputStream in = new java.net.URL(url).openStream();
-		        bitmap = BitmapFactory.decodeStream(in);
-		      } catch (Exception e) {
-		          Log.e("MyApp", e.getMessage());
-		      }
-		      return bitmap;
-		  }
+            try {
 
-		  protected void onPostExecute(Bitmap result) {
-		      bmImage.setImageBitmap(result);
-		  }
-		}
-	private int currImage = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_imagedownloader);
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                //readStream(in);
+                //InputStream in = new java.net.URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("MyApp", e.getMessage());
+            } finally {
+                urlConnection.disconnect();
+            }
+            return bitmap;
+        }
 
-		setInitialImage();
-		setImageRotateListener();
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 
-	}
+    private int currImage = 0;
 
-	private void setImageRotateListener() {
-		final Button rotatebutton = (Button) findViewById(R.id.btnRotateImage);
-		rotatebutton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				currImage++;
-				if(currImage == 2) {
-					currImage = 0;
-				}
-				setCurrentImage();
-			}
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imagedownloader);
 
-	private void setInitialImage() {
-		setCurrentImage();
-	}
+        setInitialImage();
+        setImageRotateListener();
 
-	private void setCurrentImage() {
-		final ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
-		ImageDownloader imageDownLoader = new ImageDownloader(imageView);
-		imageDownLoader.execute(imageUrls[currImage]);
-	}
+    }
+
+    private void setImageRotateListener() {
+        final Button rotatebutton = (Button) findViewById(R.id.btnRotateImage);
+        rotatebutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                currImage++;
+                if (currImage == 2) {
+                    currImage = 0;
+                }
+                setCurrentImage();
+            }
+        });
+    }
+
+    private void setInitialImage() {
+        setCurrentImage();
+    }
+
+    private void setCurrentImage() {
+        final ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
+        ImageDownloader imageDownLoader = new ImageDownloader(imageView);
+        imageDownLoader.execute(imageUrls[currImage]);
+    }
 
 }
